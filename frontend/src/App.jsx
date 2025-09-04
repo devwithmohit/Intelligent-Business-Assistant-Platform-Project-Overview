8005781ea6652684beb586adc8e3dd355f1b5304
devwithmohit <developersmohit@gmail.com>
2025-09-04 14:52:16 +0530

    feat: update package.json and pnpm-lock.yaml to add new dependencies including axios, jwt-decode, react-hook-form, and react-router-dom; create new context and hooks for authentication; add initial structure for Login and Signup pages, ProtectedRoute, and AppRoutes components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import CssBaseline from "@mui/material/CssBaseline";
-import MainLayout from "./components/layout/MainLayout"; // added
+import MainLayout from "./components/layout/MainLayout"; 
 function App() {
   return (
     <ThemeProvider theme={theme}>
