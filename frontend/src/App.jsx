1144356abe68ff77211da001548b2ca137b2df57
devwithmohit <developersmohit@gmail.com>
2025-09-06 11:23:33 +0530

    feat: add Tailwind CSS configuration file for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppRoutes from "./routes/AppRoutes";
-
+import "./index.css";
 function App() {
   return (
     <ThemeProvider theme={theme}>
